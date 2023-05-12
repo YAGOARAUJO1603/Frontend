87bef2e56fe570d1093485cec7a558495c18737d
YAGOARAUJO1603 <y.desenvolvedor1603@gmail.com>
2023-05-11 22:02:31 -0300

    [Frontend] Listar produtos e Vetor

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-produtos.component.css']
 })
 export class ListarProdutosComponent {
+  listaStrings: string[] = ['Primeiro', 'Segundo', 'Terceiro'];
+  listaNumeros: number[] = [15, 15.18, 100];
+
+  objetoModelo = {
+    nome: 'Yago',
+    idade: 21,
+    altura: 1.84,
+    graduado: true
+  };
+
+  listarProdutos: any[] = [
+    {nome: 'Curso de Angular', precoProduto: 32.36, validade: '2021-12-31', id: 1},
+    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31', id: 2, promocao: true},
+    {id: 3, nome: 'Curso de Ionic Avançado', precoProduto: 50, validade: '2021-12-31'},
+  ];
+
+  constructor(){
+    for (let item of this.listaStrings){
+      console.log(item);
+    }
+    for (const item of this.listaNumeros){
+      console.log(item);
+    }
+    console.log(this.objetoModelo);
+    console.log(this.objetoModelo.nome);
+
+   }
+
+  ngOniti(): void {
+
+  }
 
 }
